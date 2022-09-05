976202b829a4378a18093c1790282f56050b4e45
Mia Isakovic <47527694+miaisakovic@users.noreply.github.com>
2022-09-05 15:50:18 -0400

    use Flake8

--- tictactoe.py
+++ tictactoe.py
@@ -1,9 +1,9 @@
-from tkinter import *
+import tkinter as tk
 
-root = Tk()
+root = tk.Tk()
 root.title("Tic-tac-toe")
 
-blank_img = PhotoImage(width=100, height=100)
+blank_img = tk.PhotoImage(width=100, height=100)
 
 
 def button_click(pos):
@@ -11,21 +11,40 @@ def button_click(pos):
 
 
 # Create widgets
-status = Label(root, text="X plays", font=("Monaco 15"))
+status = tk.Label(root, text="X plays", font=("Monaco 15"))
 
-button_1 = Button(root, image=blank_img, highlightbackground="#24C0D6", compound="center", width=100, height=100, command=lambda: button_click(0))
-button_2 = Button(root, image=blank_img, highlightbackground="#24C0D6", compound="center", width=100, height=100, command=lambda: button_click(1))
-button_3 = Button(root, image=blank_img, highlightbackground="#24C0D6", compound="center", width=100, height=100, command=lambda: button_click(2))
+button1 = tk.Button(root, image=blank_img, highlightbackground="#24C0D6",
+                    compound="center", width=100, height=100,
+                    command=lambda: button_click(0))
+button2 = tk.Button(root, image=blank_img, highlightbackground="#24C0D6",
+                    compound="center", width=100, height=100,
+                    command=lambda: button_click(1))
+button3 = tk.Button(root, image=blank_img, highlightbackground="#24C0D6",
+                    compound="center", width=100, height=100,
+                    command=lambda: button_click(2))
 
-button_4 = Button(root, image=blank_img, highlightbackground="#24C0D6", compound="center", width=100, height=100, command=lambda: button_click(3))
-button_5 = Button(root, image=blank_img, highlightbackground="#24C0D6", compound="center", width=100, height=100, command=lambda: button_click(4))
-button_6 = Button(root, image=blank_img, highlightbackground="#24C0D6", compound="center", width=100, height=100, command=lambda: button_click(5))
+button4 = tk.Button(root, image=blank_img, highlightbackground="#24C0D6",
+                    compound="center", width=100, height=100,
+                    command=lambda: button_click(3))
+button5 = tk.Button(root, image=blank_img, highlightbackground="#24C0D6",
+                    compound="center", width=100, height=100,
+                    command=lambda: button_click(4))
+button6 = tk.Button(root, image=blank_img, highlightbackground="#24C0D6",
+                    compound="center", width=100, height=100,
+                    command=lambda: button_click(5))
 
-button_7 = Button(root, image=blank_img, highlightbackground="#24C0D6", compound="center", width=100, height=100, command=lambda: button_click(6))
-button_8 = Button(root, image=blank_img, highlightbackground="#24C0D6", compound="center", width=100, height=100, command=lambda: button_click(7))
-button_9 = Button(root, image=blank_img, highlightbackground="#24C0D6", compound="center", width=100, height=100, command=lambda: button_click(8))
+button7 = tk.Button(root, image=blank_img, highlightbackground="#24C0D6",
+                    compound="center", width=100, height=100,
+                    command=lambda: button_click(6))
+button8 = tk.Button(root, image=blank_img, highlightbackground="#24C0D6",
+                    compound="center", width=100, height=100,
+                    command=lambda: button_click(7))
+button9 = tk.Button(root, image=blank_img, highlightbackground="#24C0D6",
+                    compound="center", width=100, height=100,
+                    command=lambda: button_click(8))
 
-buttons = [button_1, button_2, button_3, button_4, button_5, button_6, button_7, button_8, button_9]
+buttons = [button1, button2, button3, button4, button5, button6,
+           button7, button8, button9]
 
 
 def click(pos):
@@ -35,7 +54,7 @@ def click(pos):
     button = buttons[pos]
 
     # Check if button has been previously clicked
-    if button["state"] == DISABLED:
+    if button["state"] == "disabled":
         pass
     else:
         try:
@@ -60,23 +79,23 @@ def click(pos):
         button["text"] = turn
         button["font"] = ("Helvetica 50 bold")
 
-        button["state"] = DISABLED
+        button["state"] = "disabled"
 
         # Check if there is a winner
         if (
-            (button_1["text"] == button_2["text"] == button_3["text"] == turn) or
-            (button_4["text"] == button_5["text"] == button_6["text"] == turn) or
-            (button_7["text"] == button_8["text"] == button_9["text"] == turn) or
-            (button_1["text"] == button_4["text"] == button_7["text"] == turn) or
-            (button_2["text"] == button_5["text"] == button_8["text"] == turn) or
-            (button_3["text"] == button_6["text"] == button_9["text"] == turn) or
-            (button_1["text"] == button_5["text"] == button_9["text"] == turn) or
-            (button_3["text"] == button_5["text"] == button_7["text"] == turn)
+            (button1["text"] == button2["text"] == button3["text"] == turn) or
+            (button4["text"] == button5["text"] == button6["text"] == turn) or
+            (button7["text"] == button8["text"] == button9["text"] == turn) or
+            (button1["text"] == button4["text"] == button7["text"] == turn) or
+            (button2["text"] == button5["text"] == button8["text"] == turn) or
+            (button3["text"] == button6["text"] == button9["text"] == turn) or
+            (button1["text"] == button5["text"] == button9["text"] == turn) or
+            (button3["text"] == button5["text"] == button7["text"] == turn)
            ):
             status["text"] = "Player " + turn + " wins!"
             i = 0
             while i < len(buttons):
-                buttons[i]["state"] = DISABLED
+                buttons[i]["state"] = "disabled"
                 i = i + 1
 
         # Check if there is a tie
@@ -87,16 +106,16 @@ def click(pos):
 # Display widgets
 status.grid(row=3, column=0, columnspan=3)
 
-button_1.grid(row=0, column=0)
-button_2.grid(row=0, column=1)
-button_3.grid(row=0, column=2)
+button1.grid(row=0, column=0)
+button2.grid(row=0, column=1)
+button3.grid(row=0, column=2)
 
-button_4.grid(row=1, column=0)
-button_5.grid(row=1, column=1)
-button_6.grid(row=1, column=2)
+button4.grid(row=1, column=0)
+button5.grid(row=1, column=1)
+button6.grid(row=1, column=2)
 
-button_7.grid(row=2, column=0)
-button_8.grid(row=2, column=1)
-button_9.grid(row=2, column=2)
+button7.grid(row=2, column=0)
+button8.grid(row=2, column=1)
+button9.grid(row=2, column=2)
 
 root.mainloop()
